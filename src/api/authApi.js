70139a38cb1080f3ab6ed547281f0c4eedf535ba
ZevCorwin <demonkill0469@gmail.com>
2025-10-30 22:00:29 +0700

    Chỉnh lại đường dẫn

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080'; // URL của backend
+const API_BASE = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
 
 export const login = async (data) => {
-  return await axios.post(`${API_URL}/login`, data);
+  return await axios.post(`${API_BASE}/login`, data);
 };
 
 export const register = async (data) => {
-  return await axios.post(`${API_URL}/register`, data);
+  return await axios.post(`${API_BASE}/register`, data);
 };
